7a9a21fe9d032da47b52514bec64c970aed33b7e
BrandonAscencio00077921 <00077921@uca.edu.sv>
2023-04-21 12:49:02 -0600

    Laboratorio 05

---
+++ Laboratorio05/app/src/main/java/com/example/laboratorio05/FirstFragment.kt
@@ -0,0 +1,49 @@
+package com.example.laboratorio05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class FirstFragment : Fragment() {
+
+    private lateinit var buttonAdd: FloatingActionButton
+    private lateinit var movie: CardView
+    private lateinit var Movie: CardView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_f_irst, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind()
+        buttonClick()
+    }
+
+    fun bind(){
+        buttonAdd = view?.findViewById(R.id.buttonAdd) as FloatingActionButton
+        movie = view?.findViewById(R.id.movie1) as CardView
+        Movie = view?.findViewById(R.id.movie2) as CardView
+    }
+    fun buttonClick(){
+        buttonAdd.setOnClickListener{
+            it.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+        movie.setOnClickListener{
+            it.findNavController().navigate(R.id.action_firstFragment_to_thirdFragment)
+        }
+        Movie.setOnClickListener{
+            it.findNavController().navigate(R.id.action_firstFragment_to_thirdFragment)
+        }
+    }
+}
